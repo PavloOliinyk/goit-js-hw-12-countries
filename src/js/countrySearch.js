46e973935de3ce1a0b8b167840bcd7ab8e75d03c
Pavlo Oliinyk <oliinyk.pavlo@gmail.com>
2021-08-01 22:07:50 +0300

    добавил реализацию pnotify

--- src/js/countrySearch.js
+++ src/js/countrySearch.js
@@ -1,4 +1,7 @@
 import '../sass/main.scss';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { success, notice, error, info, defaults } from '@pnotify/core';
 import fetchCountries from './fetchCountries';
 import getRefs from './refs';
 import countryCardTemplate from '../templates/country-card.hbs';
@@ -7,7 +10,7 @@ import countriesListTemplate from '../templates/countries-list.hbs';
 var debounce = require('lodash.debounce');
 
 const { input, countryContainer } = getRefs();
-
+defaults.delay = 1500;
 input.addEventListener('input', debounce(onSearch, 500));
 
 function onSearch(e) {
@@ -16,12 +19,13 @@ function onSearch(e) {
   countryContainer.innerHTML = '';
   const searchQuery = e.target.value;
 
-  fetchCountries(searchQuery).then(renderCountryCard).catch(onFetchError);
+  fetchCountries(searchQuery).then(renderCountryCard).catch(noticeNull);
 }
 
 function renderCountryCard(country) {
   if (country.length > 10) {
     console.log('больше 10');
+    noticeError();
     return;
   }
 
@@ -29,14 +33,41 @@ function renderCountryCard(country) {
     console.log('от 2 до 10');
     const countriesList = countriesListTemplate(country);
     countryContainer.insertAdjacentHTML('beforeend', countriesList);
+    noticeProgress();
     return;
   }
 
   console.log('1');
+
   const countryCardMarkUp = countryCardTemplate(country);
   countryContainer.insertAdjacentHTML('beforeend', countryCardMarkUp);
+  noticeSuccess();
 }
 
-function onFetchError(error) {
-  console.log(error);
+function noticeNull() {
+  notice({
+    title: 'Warning',
+    text: 'Please, type country to show',
+  });
+}
+
+function noticeError() {
+  error({
+    title: 'Error',
+    text: 'Too many matches found. Please enter a more specific query',
+  });
+}
+
+function noticeProgress() {
+  info({
+    title: 'Information',
+    text: 'Please, keep searching...',
+  });
+}
+
+function noticeSuccess() {
+  success({
+    title: 'Success',
+    text: 'The search proceeded successfully',
+  });
 }
